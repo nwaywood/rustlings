33997cb3918ef7c61fa2ed1a532fdf2e5fc1e8cb
Nick Waywood <nwaywood@au1.ibm.com>
2020-05-05 16:43:09 +1000

    Up to error handling

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,10 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+    call_me("Nick");
+}
+
+fn call_me(name : &str) {
+    println!("Call me {}", name)
 }
--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,10 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -17,8 +15,11 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+// borrow the variable so that main can still use vec0 after
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    // make a copy of the referenced data that is owned by this function
+    // (since it has to return an owned Vec and not &Vec
+    let mut vec = vec.clone();
 
     vec.push(22);
     vec.push(44);
